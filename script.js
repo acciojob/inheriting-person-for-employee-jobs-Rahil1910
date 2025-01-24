c4d4bbb6e9fd583fae97e9be2b5659ed0f61fd67
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-24 16:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Person {
                 this._jobTitle = jobTitle;
             }
             jobGreet(){
-                console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}`);
+                console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
             }
         }
 // Do not change code below this line
